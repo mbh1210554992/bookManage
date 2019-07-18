6fbe23888ee82b0b00f4fb137a9a2e1899897d96
mbh1210554992 <mbh1210554992@gmail.com>
2019-07-18 20:00:59 +0800

    CRUD

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,15 +1,14 @@
 package com.example.demo.controller;
 
 import com.example.demo.common.JsonResult;
-import com.example.demo.entity.User;
 import com.example.demo.entity.UserVO;
 import com.example.demo.service.UserLoginService;
-import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
+
 
 @Controller
 public class LoginController {
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("User")
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserService userService;
